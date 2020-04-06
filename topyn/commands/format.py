975324defab8e7a872e236cb7ff566312f3f8d5b
leandro.leites <leandro.leites@flixbus.com>
2020-04-06 18:28:12 +0200

    Added topyn config for unified handling of exclusions

--- topyn/commands/format.py
+++ topyn/commands/format.py
@@ -1,15 +1,23 @@
 from typing import List
 
-from topyn.commands import run_command, get_config
+from topyn.commands import run_command, get_config, get_topyn_excludes
 
 
 def _extra_args(fix: bool, module: str) -> List[str]:
-
+    excludes = "|".join(
+        [
+            exclude if exclude[0] != "." else f"\\{exclude}"
+            for exclude in get_topyn_excludes()
+        ]
+    )
     config_path = get_config(module)
     extra_args = [
         "--config",
         str(config_path.resolve()),
+        "--exclude",
+        f"/({excludes})/",
     ]
+
     if not fix:
         extra_args.append("--check")
 
