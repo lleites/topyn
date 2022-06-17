f3b85be84704a178b64804ee8b6fff98d6b1476a
leandro.leites <laloleites@gmail.com>
2022-06-17 11:54:32 +0200

    Fixing format of test file

--- tests/test_topyn.py
+++ tests/test_topyn.py
@@ -57,7 +57,10 @@ def test_incorrect_naming(capsys: CaptureFixture) -> None:
 
         mock_out.assert_called_with("rules")
         captured = capsys.readouterr()
-        assert "N802 function name 'iLikeCamelCase' should be lowercase" in captured.out
+        assert (
+            "N802 function name 'iLikeCamelCase' should be lowercase"
+            in captured.out
+        )
 
 
 def test_bugbear(capsys: CaptureFixture) -> None:
@@ -68,7 +71,10 @@ def test_bugbear(capsys: CaptureFixture) -> None:
 
         mock_out.assert_called_with("rules")
         captured = capsys.readouterr()
-        assert "B006 Do not use mutable data structures for argument defaults" in captured.out
+        assert (
+            "B006 Do not use mutable data structures for argument defaults"
+            in captured.out
+        )
 
 
 def test_comprehensions(capsys: CaptureFixture) -> None:
@@ -96,10 +102,15 @@ def test_wrong_formatting(capsys: CaptureFixture) -> None:
 def test_ok(capsys: CaptureFixture) -> None:
     import topyn.tui
 
-    with patch.object(topyn.tui, "everything_is_ok", wraps=topyn.tui.everything_is_ok) as mock_out:
+    with patch.object(
+        topyn.tui, "everything_is_ok", wraps=topyn.tui.everything_is_ok
+    ) as mock_out:
         run(["tests/resources/ok"])
         mock_out.assert_called_once()
-        assert "All done! ✨ 🍰 ✨\n1 file would be left unchanged.\n" == capsys.readouterr().err
+        assert (
+            "All done! ✨ 🍰 ✨\n1 file would be left unchanged.\n"
+            == capsys.readouterr().err
+        )
 
 
 def test_fix(tmpdir: py.path.local) -> None:
@@ -112,7 +123,9 @@ def test_fix(tmpdir: py.path.local) -> None:
         _run_system_exit(str(tmpdir))
         mock_out.assert_called_with("formatting")
 
-    with patch.object(topyn.tui, "trying_to_fix", wraps=topyn.tui.trying_to_fix) as mock_out:
+    with patch.object(
+        topyn.tui, "trying_to_fix", wraps=topyn.tui.trying_to_fix
+    ) as mock_out:
         run([str(tmpdir), "--fix"])
         mock_out.assert_called_once()
 
@@ -131,7 +144,9 @@ def test_fix_rules(tmpdir: py.path.local) -> None:
         _run_system_exit(str(tmpdir))
         mock_out.assert_called_with("rules")
 
-    with patch.object(topyn.tui, "trying_to_fix", wraps=topyn.tui.trying_to_fix) as mock_out:
+    with patch.object(
+        topyn.tui, "trying_to_fix", wraps=topyn.tui.trying_to_fix
+    ) as mock_out:
         run([str(tmpdir), "--fix"])
         mock_out.assert_called_once()
 
